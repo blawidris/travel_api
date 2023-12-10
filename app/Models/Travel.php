dd2e2222aa8c8f4f2a7672329e1c22cbd53e3026
elisaMargret <e04091069@gmail.com>
2023-12-10 10:46:56 +0100

    database setup

---
+++ app/Models/Travel.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Travel extends Model
+{
+    use HasFactory, Sluggable;
+
+    protected $table = "travels";
+
+    protected $fillable = [
+        "name", 'number_of_days', 'description', 'is_public', 'slug'
+    ];
+
+
+    public function tours(): HasMany
+    {
+        return $this->hasMany(Tour::class);
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+    public function numberOfNights(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value, $attribute) => $attribute['number_of_days'] - 1
+        );
+    }
+
+   
+}
